3c58f73d5756c50b355a85b76850195b20f0d614
Bhuvan Perakam <Bhuvan_Perakam@epam.com>
2018-07-19 12:07:22 +0530

    Add project files.

---
+++ Ericsson/BinarySearchTree.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ericsson
+{
+    // Less than the root Node value will be left side node and Greater than the root Node value will be right side node
+    class BinarySearchTree
+    {
+        public static Node InsertNode(Node node, int value)
+        {
+            if (node == null)
+                return new Node(value);
+
+            if (value > node.value)
+            {
+                if (node.right == null)
+                {
+                    node.right = new Node(value);
+                    return node;
+                }
+                else
+                    InsertNode(node.right, value);
+            }
+            else if (value < node.value)
+            {
+                if (node.left == null)
+                {
+                    node.left = new Node(value);
+                    return node;
+                }
+                else
+                    InsertNode(node.left, value);
+            }
+
+            return node;
+        }
+
+        public static bool SearchNode(Node node, int value)
+        {
+            bool isSearch = false;
+            if (node == null)
+                return false;
+
+            if (node != null && node.value == value)
+                return true;
+
+            if (value > node.value)
+                SearchNode(node.right, value);
+            if (value < node.value)
+                SearchNode(node.left, value);
+
+            return isSearch;
+        }
+
+        // Left - Root - Right
+        public static void PrintInOrderTraversal(Node node)
+        {
+            if (node != null)
+            {
+                PrintInOrderTraversal(node.left);
+                Console.Write("\t {0}", node.value);
+                PrintInOrderTraversal(node.right);
+            }
+        }
+
+        //Root - Left - Right
+        public static void PrintPreOrderTraversal(Node node)
+        {
+            if (node != null)
+            {
+                Console.Write("\t {0}", node.value);
+                PrintPreOrderTraversal(node.left);
+                PrintPreOrderTraversal(node.right);
+            }
+        }
+
+        // Left - Right - Root
+        public static void PrintPostOrderTraversal(Node node)
+        {
+            if (node != null)
+            {
+                PrintPostOrderTraversal(node.left);
+                PrintPostOrderTraversal(node.right);
+                Console.Write("\t {0}", node.value);
+            }
+        }
+
+        /*
+         * All Level Order - 14, 5, 30, 4, 16, 15, 35
+         */
+        public static void PrintAllLevelOrderTraversal(Node node)
+        {
+            int height = HeightofTheTree(node);
+            for (int i = 0; i < height; i++)
+            {
+                Console.WriteLine("\n");
+                PrintValues(node, i);
+            }
+        }
+
+        private static void PrintValues(Node node, int level)
+        {
+            if (node == null)
+                return;
+
+            if (level == 0)
+                Console.Write("\t {0}", node.value);
+            else
+            {
+                PrintValues(node.left, level - 1);
+                PrintValues(node.right, level - 1);
+            }
+        }
+
+
+        private static int HeightofTheTree(Node node)
+        {
+            if (node == null)
+                return 0;
+            else
+            {
+                int left = HeightofTheTree(node.left);
+                int right = HeightofTheTree(node.right);
+
+                if (left > right)
+                    return left + 1;
+                else
+                    return right + 1;
+            }
+        }
+
+        public int FindTheCommonNode(int aValue,int bValue)
+        {
+            int commonNode = -1;
+
+            return commonNode;
+        }
+
+
+    }
+
+    public class Node
+    {
+        public Node left;
+        public Node right;
+        public int value;
+        public Node(int _value)
+        {
+            value = _value;
+        }
+    }
+}
---
+++ Ericsson/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ericsson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node rootNode = new Node(14);
+
+            BinarySearchTree.InsertNode(rootNode, 30);
+            BinarySearchTree.InsertNode(rootNode, 15);
+            BinarySearchTree.InsertNode(rootNode, 35);
+            BinarySearchTree.InsertNode(rootNode, 5);
+            BinarySearchTree.InsertNode(rootNode, 4);
+            BinarySearchTree.InsertNode(rootNode, 6);
+
+            BinarySearchTree.PrintAllLevelOrderTraversal(rootNode);
+            Console.ReadKey();
+            //Console.WriteLine("\n");
+            //BinarySearchTree.PrintInOrderTraversal(rootNode);
+            //Console.WriteLine("\n");
+            //BinarySearchTree.PrintPreOrderTraversal(rootNode);
+            //Console.WriteLine("\n");
+            //BinarySearchTree.PrintPostOrderTraversal(rootNode);
+            //Console.ReadKey();
+
+
+
+
+            //PossibleStringPallindrome oPossibleStringPallindrome = new PossibleStringPallindrome();
+            //oPossibleStringPallindrome.PerformStringCombinations("level");
+
+
+            /*
+            MergeSortedArray oMergeSortedArray = new MergeSortedArray();
+            int[] aSortedArray = { 1, 3, 5, 7, 9 };
+            int[] bSortedArray = { 2, 4, 6, 8, 10 };
+            int[] result = oMergeSortedArray.MergeSortedArrays(aSortedArray, bSortedArray);
+            foreach (var itemResult in result)
+            {
+                Console.Write("\t {0}", itemResult);
+            }
+            */
+
+            /*
+            GenericList<int> iGenericList = new GenericList<int>();
+            iGenericList.Add(1);
+            iGenericList.Add(2);
+            iGenericList.Add(3);
+            iGenericList.Add(4);
+            iGenericList.Add(5);
+            iGenericList.PrintValues();
+            iGenericList.Add(6);
+            iGenericList.PrintValues();            
+            iGenericList.Remove(5);
+            iGenericList.PrintValues();
+            */
+
+            /*
+            GenericStack<int> iGenericStack = new GenericStack<int>();
+            iGenericStack.Push(1);
+            iGenericStack.Push(2);
+            iGenericStack.Push(3);
+            iGenericStack.Push(4);
+            iGenericStack.Push(5);
+            iGenericStack.Print();
+            iGenericStack.Push(6);
+            iGenericStack.Print();
+            iGenericStack.Pop();
+            iGenericStack.Print();
+            Console.ReadKey();
+            */
+
+            /*
+            GenericDictionary<int, int> gDictionary = new GenericDictionary<int, int>();
+            gDictionary.Add(1, 1);
+            gDictionary.Add(2, 2);
+            gDictionary.Add(3, 3);
+            gDictionary.Add(4, 4);
+            gDictionary.Add(5, 5);
+            gDictionary.print();
+            gDictionary.Add(6, 5);
+            gDictionary.print();
+            gDictionary.Remove(6);
+            gDictionary.Remove(1);
+            gDictionary.Remove(3);
+            gDictionary.print();
+            Console.ReadKey();
+            */
+
+            /*
+            GenericQueue<int> queueData = new GenericQueue<int>();
+            queueData.Enqueue(1);
+            queueData.Enqueue(2);
+            queueData.Enqueue(3);
+            queueData.Enqueue(4);
+            queueData.Enqueue(5);
+            queueData.Print();
+            queueData.Dequeue();
+            queueData.Dequeue();
+            queueData.Print();
+            Console.ReadKey();
+            */
+        }
+    }
+}
